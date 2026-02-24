5431896c3a1e4aaa07bd1ae0faf0e18aa9e02ec9
Matheus Siqueira <matheus.siqueira1137@gmail.com>
2026-02-24 14:27:42 -0300

    Melhorado o sistema de menu sandwich, aparecendo somente se estiver em um dispositivo móvel, e suavizando animações de links.

--- menu.js
+++ menu.js
@@ -1,15 +1,16 @@
-let btnmenu = document.getElementById('abrir_button_menu');
-let menu = document.getElementById('menu_mobile');
-let fecharOverlay = document.getElementById('overlay_menu'); 
+/*SCRIPT DO MENU MOBILE*/
+let btnmenu = document.getElementById("abrir_button_menu");
+let menu = document.getElementById("menu_mobile");
+let fecharOverlay = document.getElementById("overlay_menu");
 
-btnmenu.addEventListener('click', () =>{
-    menu.classList.add('btn-abrir-menu-mobile');
+btnmenu.addEventListener("click", () => {
+  menu.classList.add("btn-abrir-menu-mobile");
 });
 
-menu.addEventListener('click', () =>{
-    menu.classList.remove('btn-abrir-menu-mobile');
+menu.addEventListener("click", () => {
+  menu.classList.remove("btn-abrir-menu-mobile");
 });
 
-fecharOverlay.addEventListener('click', () =>{
-    menu.classList.remove('btn-abrir-menu-mobile');
+fecharOverlay.addEventListener("click", () => {
+  menu.classList.remove("btn-abrir-menu-mobile");
 });
